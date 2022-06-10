74faf35962d133d130745c8ebe49deb3c92848f9
Glebfra <gleb.frantsuzov@yandex.ru>
2022-06-10 22:11:32 +0300

    0.1/beta

--- VkBot.py
+++ VkBot.py
@@ -1,7 +1,5 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-import requests
-import json
 from datetime import datetime
 
 
@@ -10,7 +8,11 @@ class VkBot(object):
         self._token = token
         self.vk = vk_api.VkApi(token=token)
         self.longpool = VkLongPoll(self.vk)
-        self.commands = ['/schedule']
+
+        self.pool = {'Расписание': '/schedule', 'Неделя': '/week'}
+        self.answers = {'/schedule': 'Вот твое расписание!', '/week': 'Сейчас'}
+        self.commands = ['/schedule', '/week', '/help']
+        self.urls = {'/schedule': 'https://vk.com/club213831540?z=photo-213831540_457239017%2Falbum-213831540_00%2Frev'}
 
     @classmethod
     def create_bot(cls):
@@ -20,8 +22,9 @@ class VkBot(object):
                 token += temp_token
         return cls(token=token)
 
-    def write_msg(self, user_id, msg):
-        self.vk.method('messages.send', {'user_id': user_id, 'message': msg, 'random_id': 0})
+    def write_msg(self, user_id, msg, attachment=None):
+        self.vk.method('messages.send', {'user_id': user_id, 'message': msg,
+                                         'random_id': 0, 'attachment': attachment})
 
     def msg_loop(self):
         for event in self.longpool.listen():
@@ -29,8 +32,21 @@ class VkBot(object):
                 request = event.text.capitalize()
                 if request in self.commands:
                     eval(f'self.command_{request.removeprefix("/")}({event.user_id})')
+                elif request in self.pool:
+                    eval(f'self.command_{self.pool[request].removeprefix("/")}({event.user_id})')
                 else:
                     self.write_msg(event.user_id, 'Непонятная мне команда :-(')
+
+    def command_help(self, user_id):
+        self.write_msg(user_id, f'Сейчас доступны такие команды: {self.commands}')
+
+    def command_schedule(self, user_id):
+        self.write_msg(user_id, self.answers['/schedule'], attachment=self.urls['/schedule'])
+
+    def command_week(self, user_id):
+        date = datetime.today().isocalendar()[1]
+        start_date = datetime(2022, 9, 1).isocalendar()[1]
+        self.write_msg(user_id, f'Сейчас идет: {date - start_date + 1} неделя')
 
 
 if __name__ == '__main__':
